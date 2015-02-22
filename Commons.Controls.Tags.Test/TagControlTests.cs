54cffad2e7766397e42e70ba12bca486c3041c8a
Chui Tey <Chui.Tey@readify.net>
2015-02-22 10:51:36 +1000

    Initial

---
+++ Commons.Controls.Tags.Test/TagControlTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Commons.Controls.Tags.Test
+{
+    [TestClass]
+    public class TagControlTests: SilverlightTest
+    {
+        [TestMethod]
+        [Asynchronous]
+        public void CreateTagItemControl()
+        {
+            var tagItemControl = new TagItem { Content = "Monranbah Wines" };
+            TestPanel.Children.Add(tagItemControl);
+            tagItemControl.OnDelete += (s, e) =>
+            {
+                EnqueueTestComplete();
+            };
+            
+        }
+
+        [TestMethod]
+        public void CreateTagItemControls()
+        {
+            
+        }
+
+    }
+}
---
+++ Commons.Controls.Tags/TagItem.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Commons.Controls.Tags
+{
+    [TemplatePart(Name=PART_deleteButton, Type=typeof(Button))]
+    public partial class TagItem : ContentControl
+    {
+
+        public const string PART_deleteButton = "deleteButton";
+
+        public TagItem()
+        {
+            DefaultStyleKey = typeof(TagItem);
+
+            this.Loaded += TagItem_Loaded;
+            this.Unloaded += TagItem_Unloaded;
+        }
+
+        public event EventHandler<RoutedEventArgs> OnDelete;
+
+        #region Interaction
+        
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            SubscribeEvents();
+        }
+
+        void TagItem_Unloaded(object sender, RoutedEventArgs e)
+        {
+            //UnsubscribeEvents();
+        }
+
+        void TagItem_Loaded(object sender, RoutedEventArgs e)
+        {
+            //SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+
+            var layoutRoot = GetTemplateChild("LayoutRoot") as Grid;
+            layoutRoot.MouseEnter += layoutRoot_MouseEnter;
+            layoutRoot.MouseLeave += layoutRoot_MouseLeave;
+
+            var deleteButton = GetTemplateChild("deleteButton") as Button;
+            deleteButton.Click += deleteButton_Click;
+            
+        }
+
+        void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (OnDelete != null)
+            {
+                OnDelete(sender, e);
+            }
+        }
+
+        private void UnsubscribeEvents()
+        {
+
+            var layoutRoot = GetTemplateChild("LayoutRoot") as Grid;
+            layoutRoot.MouseEnter -= layoutRoot_MouseEnter;
+            layoutRoot.MouseLeave -= layoutRoot_MouseLeave;
+        }
+
+        void layoutRoot_MouseLeave(object sender, MouseEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "OnMouseOut", true);
+        }
+
+        void layoutRoot_MouseEnter(object sender, MouseEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "OnMouseOver", true);
+        }
+
+        #endregion
+        
+ 
+    }
+}
